a6dc9a1f19718a25a77cb039c8736e10e267f497
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-07-07 12:29:29 +0200

    Add library function from linyinfeng
    See here: https://github.com/divnix/digga/issues/464#issuecomment-1154974631

---
+++ lib/overlay-null-protector.nix
@@ -0,0 +1,9 @@
+# copied from: https://github.com/linyinfeng/dotfiles/blob/main/lib/overlay-null-protector.nix
+
+overlay:
+
+final: prev:
+
+if prev == null || (prev.isFakePkgs or false)
+then { }
+else overlay final prev
