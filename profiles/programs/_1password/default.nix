96cb51f48cd63f9287af647fead700b1eb13f163
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-08-15 21:07:15 +0200

    Remove gid option from 1password profile

--- profiles/programs/_1password/default.nix
+++ profiles/programs/_1password/default.nix
@@ -1,14 +1,11 @@
 {
-  # FIXME gid setting will be obsolete once this is merged: https://github.com/NixOS/nixpkgs/pull/172058
   programs = {
     _1password = {
       enable = true;
-      gid = 31002;
     };
 
     _1password-gui = {
       enable = true;
-      gid = 31001;
       # TODO is there a way to get the name?
       polkitPolicyOwners = [ "roberto" ];
     };
