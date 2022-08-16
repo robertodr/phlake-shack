724cf266eb8bd01e6c36d3540fa414ea2d77ee1e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-08-16 16:10:58 +0200

    Add more fonts
    Fira Code Retina is in the nerdfonts package!

--- profiles/fonts/common.nix
+++ profiles/fonts/common.nix
@@ -11,21 +11,33 @@
     fontDir.enable = true;
     fonts = with pkgs;
       [
+        bakoma_ttf
+        comfortaa
+        corefonts
+        dejavu_fonts
         emacs-all-the-icons-fonts
-
         fira
         fira-code
         fira-code-symbols
         fira-mono
-
-        corefonts
-        inconsolata
-        liberation_ttf
-        dejavu_fonts
-        bakoma_ttf
         gentium
-        ubuntu_font_family
+        gyre-fonts
+        inconsolata
+        latinmodern-math
+        liberation_ttf
+        material-design-icons
+        material-icons
+        nerdfonts
+        open-sans
         terminus_font
+        tex-gyre-math.bonum
+        tex-gyre-math.pagella
+        tex-gyre-math.schola
+        tex-gyre-math.termes
+        ubuntu_font_family
+        ubuntu_font_family
+        unifont
+        xits-math
       ];
   };
 }
