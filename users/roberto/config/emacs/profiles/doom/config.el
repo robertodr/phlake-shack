d02b6d9b01fab71ba388df64d7293483e7c0781a
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-08-16 08:34:08 +0200

    Use regular Fira Code
    For some reason the 'Retina' variant is not installed...

--- users/roberto/config/emacs/profiles/doom/config.el
+++ users/roberto/config/emacs/profiles/doom/config.el
@@ -23,9 +23,9 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 (setq
-  doom-font (font-spec :family "Fira Code Retina" :size 18 :weight 'normal :width 'normal)
-  doom-variable-pitch-font (font-spec :family "Fira Code Retina")
-  doom-big-font (font-spec :family "Fira Code Retina" :size 22))
+  doom-font (font-spec :family "Fira Code" :size 18 :weight 'normal :width 'normal)
+  doom-variable-pitch-font (font-spec :family "Fira Code")
+  doom-big-font (font-spec :family "Fira Code" :size 22))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
