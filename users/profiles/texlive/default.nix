3951c25fad90d4eddcf74bd74ea4179f96295809
Roberto Di Remigio <roberto@totaltrash.xyz>
2022-08-30 16:33:40 +0000

    FIx texlive user profile

--- users/profiles/texlive/default.nix
+++ users/profiles/texlive/default.nix
@@ -1,37 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.texlive = {
     enable = true;
     packageSet = pkgs.texlive;
     extraPackages = tpkgs: {
-    pkgFilter = pkg: (lib.elem pkg.tlType ["run" "bin"]) || pkg.pname = "core";
-    inherit (tpkgs)
-            collection-basic
-            collection-bibtexextra
-            collection-binextra
-            collection-context
-            collection-fontsextra
-            collection-fontsrecommended
-            collection-fontutils
-            collection-formatsextra
-            collection-langenglish
-            collection-langeuropean
-            collection-langfrench
-            collection-langitalian
-            collection-langother
-            collection-latex
-            collection-latexextra
-            collection-latexrecommended
-            collection-luatex
-            collection-mathscience
-            collection-metapost
-            collection-pictures
-            collection-plaingeneric
-            collection-pstricks
-            collection-publishers
-            collection-xetex
-            ;
+      pkgFilter = pkg: (lib.elem pkg.tlType [ "run" "bin" ]) || (pkg.pname == "core");
+      inherit (tpkgs)
+        collection-basic
+        collection-bibtexextra
+        collection-binextra
+        collection-context
+        collection-fontsextra
+        collection-fontsrecommended
+        collection-fontutils
+        collection-formatsextra
+        collection-langenglish
+        collection-langeuropean
+        collection-langfrench
+        collection-langitalian
+        collection-langother
+        collection-latex
+        collection-latexextra
+        collection-latexrecommended
+        collection-luatex
+        collection-mathscience
+        collection-metapost
+        collection-pictures
+        collection-plaingeneric
+        collection-pstricks
+        collection-publishers
+        collection-xetex
+        ;
     };
   };
 }
