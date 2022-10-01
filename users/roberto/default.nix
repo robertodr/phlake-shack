00a6ae847c82c38dcb86a7b53e81109ed39a5a3e
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-01 15:30:37 +0200

    Add `roberto` to more groups

--- users/roberto/default.nix
+++ users/roberto/default.nix
@@ -3,13 +3,24 @@
   home-manager.users = { inherit (hmUsers) roberto; };
 
   users.users.roberto = {
-    description = "Roberto Di Remigio";
+    description = "Roberto Di Remigio Eikås";
     isNormalUser = true;
     home = "/home/roberto";
     createHome = true;
     hashedPassword = "$6$YoVa.tQqReqjgAav$KJ3u/TyGtsAiIpAqXbFqtRKjltfKsx5A5GSJLzP0zxMH1h6SeW.9aYEHB0gzjbgNkK.sIJDxaXdy/BYdLAVZu/";
     extraGroups = [
+      "adm"
+      "audio"
+      "disk"
       "docker"
+      "docker"
+      "networkmanager"
+      "root"
+      "systemd-journal"
+      "users"
+      "vboxusers"
+      "video"
+      "wheel"
       "wheel"
     ];
     shell = pkgs.fish;
