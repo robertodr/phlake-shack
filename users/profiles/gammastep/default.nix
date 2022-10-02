694709a605aa8e349dc488a218fa442f26de9d66
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-02 14:45:01 +0200

    Add gammastep configuration

---
+++ users/profiles/gammastep/default.nix
@@ -0,0 +1,12 @@
+{
+  services.gammastep = {
+    enable = true;
+    latitude = "59.329323";
+    longitude = "18.068581";
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+    tray = enable;
+  };
+}
