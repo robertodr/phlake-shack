25b9ec5823594f1b2e9e332d54dbb7c98e4aec59
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-02 14:56:40 +0200

    Add swayidle configuration

---
+++ users/profiles/swayidle/default.nix
@@ -0,0 +1,17 @@
+moduleArgs @ { pkgs
+, ...
+}:
+
+{
+  services.swayidle = {
+    enable = true;
+    events = [
+      { event = "before-sleep"; command = "swaylock"; }
+      { event = "lock"; command = "lock"; }
+    ];
+  };
+
+  home.packages = with pkgs; [
+    swaylock
+  ];
+}
