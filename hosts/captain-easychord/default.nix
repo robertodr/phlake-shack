51119a959d255a505398e6fed4e1fb99810bfec0
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-02 15:42:59 +0200

    Add TLP configuration to host file
    The parameters are host-specific!

--- hosts/captain-easychord/default.nix
+++ hosts/captain-easychord/default.nix
@@ -9,7 +9,6 @@
   ++ suites.i3
   ++ suites.multimedia
   ++ suites.virtualisation
-  ++ (with profiles.services; [ tlp ])
   ++ (with profiles.users; [
     roberto
   ])
@@ -54,6 +53,21 @@
 
   # NOTE fprintd is enabled by default on the Framework!
   services.fprintd.enable = false;
+
+  powerManagement.powertop.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+      START_CHARGE_THRESH_BAT0 = 90;
+      STOP_CHARGE_THRESH_BAT0 = 97;
+      RUNTIME_PM_ON_BAT = "auto";
+      PCIE_ASPM_ON_BAT = "powersupersave";
+    };
+  };
 
   system = {
     # This value determines the NixOS release from which the default
