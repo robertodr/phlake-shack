a0af7caa075d85f814d4304c3130978e7e93aeef
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-02 16:40:09 +0200

    Add sway top-level, remove xdg
    Needed to get greetd to work!

---
+++ profiles/programs/sway/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+    ];
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+}
+
