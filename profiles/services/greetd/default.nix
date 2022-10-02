6a5b1a2f9308cc6024d2fd95eb32779dcde1cefd
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-02 15:29:31 +0200

    Add xdg and greetd configurations

---
+++ profiles/services/greetd/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+
+  services.greetd = {
+    enable = true;
+    package = pkgs.greetd.gtkgreet;
+    settings = {
+      default_session.command = "sway --config /etc/greetd/sway-config";
+    };
+  };
+
+  security.pam.services.greetd.enableGnomeKeyring = true;
+
+  environment.etc = {
+    "greetd/environments".text = ''
+      sway
+      bash
+    '';
+    "greetd/sway-config".text = ''
+      # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+      exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+
+      input type:keyboard xkb_layout it(us)
+
+      bindsym Mod4+shift+e exec swaynag \
+        -t warning \
+        -m 'What do you want to do?' \
+        -b 'Poweroff' 'systemctl poweroff' \
+        -b 'Reboot' 'systemctl reboot'
+
+      include /etc/sway/config.d/*
+    '';
+  };
+}
---
+++ profiles/services/xdg/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+  };
+}
+
+
