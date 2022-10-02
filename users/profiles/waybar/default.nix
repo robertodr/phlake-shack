a0f04597385000c7ee9ca61ecb6b7e4fe6ba6642
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-02 15:03:33 +0200

    Add initial waybar configuration

---
+++ users/profiles/waybar/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  programs.waybar = {
+    enable = true;
+
+    systemd.enable = true;
+
+    package = pkgs.waybar.override {
+      withMediaPlayer = true;
+    };
+
+    # TODO add fancy configuration :)
+  };
+}
