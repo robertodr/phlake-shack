27eef746817c92fd2f3e4f749b8e57aa3c9fd504
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-08 18:57:08 +0200

    Adjust swayidle configuration

--- users/profiles/swayidle/default.nix
+++ users/profiles/swayidle/default.nix
@@ -2,13 +2,37 @@ moduleArgs @ {pkgs, ...}: {
   services.swayidle = {
     enable = true;
     events = [
+      # lock displays before going to sleep
       {
         event = "before-sleep";
-        command = "swaylock";
+        command = ''
+          swaylock -k -l
+        '';
       }
+
+      # turn displays back on when resuming
       {
-        event = "lock";
-        command = "lock";
+        event = "resume";
+        command = ''
+          swaymsg 'output * dpms on'
+        '';
+      }
+    ];
+    timeouts = [
+      # lock after 60 seconds idle
+      {
+        timeout = 60;
+        command = ''
+          swaylock -k -l
+        '';
+      }
+
+      # turn off displays after 120 seconds idle
+      {
+        timeout = 120;
+        command = ''
+          swaymsg 'output * dpms off'
+        '';
       }
     ];
   };
