7242f0430948b95499cb9fb07e7adcbf8d8e9bea
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-10 10:29:07 +0200

    Geneate blurred/flopped backgrounds

--- pkgs/pulse-demon/default.nix
+++ pkgs/pulse-demon/default.nix
@@ -1,14 +1,21 @@
-{ stdenv
+{
+  stdenv,
+  pkgs,
 }:
-
 stdenv.mkDerivation {
   pname = "pulse-demon-background";
   version = "1.0";
 
   src = ./.;
 
+  nativeBuildInputs = [pkgs.imagemagick];
+
   installPhase = ''
     mkdir -p $out/share
-    cp $src/*.png $out/share
+    cp $src/pulse-demon.png $out/share
+    # motion blur
+    ${pkgs.imagemagick}/bin/convert $src/pulse-demon.png -motion-blur 0x5+1.5 $out/share/blur-pulse-demon.png
+    # motion blur and flop
+    ${pkgs.imagemagick}/bin/convert $src/pulse-demon.png -motion-blur 0x5+0.5 -flop $out/share/flop-blur-pulse-demon.png
   '';
 }
