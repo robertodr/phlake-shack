008b05d12a20a4eeb409f10c9332323bf597c2a9
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-23 17:52:22 +0200

    Configure kanshi for home docking
    Quite painless, compared to autorandr...

--- users/profiles/kanshi/default.nix
+++ users/profiles/kanshi/default.nix
@@ -1,5 +1,42 @@
-{...}: {
+{pkgs, ...}: {
   services.kanshi = {
     enable = true;
+
+    profiles = {
+      undocked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            position = "0,0";
+            mode = "2256x1504";
+            scale = 1.5;
+          }
+        ];
+      };
+
+      docked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            position = "2560,260";
+            mode = "2256x1504";
+            scale = 1.5;
+          }
+          {
+            criteria = "DP-6";
+            mode = "2560x1440";
+            position = "0,0";
+            scale = 1.0;
+          }
+        ];
+
+        exec = [
+          "${pkgs.sway}/bin/swaymsg workspace number 1, move workspace to DP-6"
+          "${pkgs.sway}/bin/swaymsg workspace number 2, move workspace to DP-6"
+          "${pkgs.sway}/bin/swaymsg workspace number 3, move workspace to eDP-1"
+          "${pkgs.sway}/bin/swaymsg workspace number 4, move workspace to eDP-1"
+        ];
+      };
+    };
   };
 }
