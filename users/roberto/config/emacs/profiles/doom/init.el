c0bdb3fede2e5adb167a8b2d0f0925beefa62221
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-23 17:51:17 +0200

    Add `tree-sitter` in Emacs

--- users/roberto/config/emacs/profiles/doom/init.el
+++ users/roberto/config/emacs/profiles/doom/init.el
@@ -116,7 +116,7 @@
   :lang
   ;;agda              ; types of types of types of types...
   ;;beancount         ; mind the GAAP
-  (cc +lsp)           ; C > C++ == 1
+  (cc +lsp +tree-sitter)  ; C > C++ == 1
   ;;clojure           ; java with a lisp
   ;;common-lisp       ; if you've seen one lisp, you've seen them all
   ;;coq               ; proofs-as-programs
@@ -158,7 +158,7 @@
   ;;php               ; perl's insecure younger brother
   ;;plantuml          ; diagrams for confusing people more
   ;;purescript        ; javascript, but functional
-  (python +lsp +pyright)            ; beautiful is better than ugly
+  (python +lsp +pyright +tree-sitter)  ; beautiful is better than ugly
   ;;qt                ; the 'cutest' gui framework ever
   ;;racket            ; a DSL for DSLs
   ;;raku              ; the artist formerly known as perl6
