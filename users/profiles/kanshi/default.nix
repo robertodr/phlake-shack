793badde91d72c275699893e5f9a1819f766255a
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-25 15:35:19 +0200

    Configure kanshi for KTH desk

--- users/profiles/kanshi/default.nix
+++ users/profiles/kanshi/default.nix
@@ -37,6 +37,30 @@
           "${pkgs.sway}/bin/swaymsg workspace number 4, move workspace to eDP-1"
         ];
       };
+
+      kth = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            position = "2560,260";
+            mode = "2256x1504";
+            scale = 1.5;
+          }
+          {
+            criteria = "DP-3";
+            mode = "2560x1440";
+            position = "0,0";
+            scale = 1.0;
+          }
+        ];
+
+        exec = [
+          "${pkgs.sway}/bin/swaymsg workspace number 1, move workspace to DP-3"
+          "${pkgs.sway}/bin/swaymsg workspace number 2, move workspace to DP-3"
+          "${pkgs.sway}/bin/swaymsg workspace number 3, move workspace to eDP-1"
+          "${pkgs.sway}/bin/swaymsg workspace number 4, move workspace to eDP-1"
+        ];
+      };
     };
   };
 }
