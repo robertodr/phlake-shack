bf8d7a1bcd0f3e3fdae47ef10a9b72a93cfd7338
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-10-26 21:10:40 +0200

    Walk back on attempt to package openconnect-sso

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,7 @@
 final: prev: {
   # keep sources this first
-  sources = prev.callPackage (import ./_sources/generated.nix) { };
+  sources = prev.callPackage (import ./_sources/generated.nix) {};
 
   # then, call packages with `final.callPackage`
-  pulse-demon = final.callPackage ./pulse-demon { };
-
-  #openconnect-sso = final.libsForQt5.callPackage ./openconnect-sso {
-  #  inherit (sources);
-  #};
+  pulse-demon = final.callPackage ./pulse-demon {};
 }
