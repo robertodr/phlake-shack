bde73f2ddd54a37cb9884d2da43bc941482b6619
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-11-16 19:03:59 +0100

    Try to add virtual camera

--- hosts/captain-easychord/default.nix
+++ hosts/captain-easychord/default.nix
@@ -28,7 +28,28 @@
 
     kernelPackages = pkgs.linuxPackages_latest;
 
-    # Bootloader.
+    # make some extra kernel modules available to NixOS
+    extraModulePackages = [
+      config.boot.kernelPackages.v4l2loopback.out
+    ];
+
+    # activate kernel modules (choose from built-ins and extra ones)
+    kernelModules = [
+      # Virtual Camera
+      "v4l2loopback"
+      # Virtual Microphone, built-in
+      "snd-aloop"
+    ];
+
+    # initial kernel module settings
+    extraModprobeConfig = ''
+      # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+      # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+      # https://github.com/umlaeute/v4l2loopback
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
+
+    # bootloader
     loader = {
       systemd-boot.enable = true;
       efi = {
