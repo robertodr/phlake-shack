77517a48c76ad9930c1502cd51e17dca4e57e6bd
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-11-19 10:57:39 +0100

    Do not install enso

--- users/profiles/core.nix
+++ users/profiles/core.nix
@@ -92,7 +92,7 @@ in {
       cmake-language-server
       delta # TODO <- this or difftastic?
       dive
-      nur.repos.robertodr.enso
+      #nur.repos.robertodr.enso
       flamegraph
       nur.repos.robertodr.git-along
       git-extras
