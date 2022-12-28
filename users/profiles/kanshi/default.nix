49ac439197db1f49430065e6fff3c7ba6f579015
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-12-28 13:31:03 +0100

    Add one more external screen configuration

--- users/profiles/kanshi/default.nix
+++ users/profiles/kanshi/default.nix
@@ -85,6 +85,30 @@
           "${pkgs.sway}/bin/swaymsg workspace number 4, move workspace to eDP-1"
         ];
       };
+
+      bergen = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            mode = "2256x1504";
+            position = "1920,100";
+            scale = 1.5;
+          }
+          {
+            criteria = "Hewlett Packard HP E242 CN472419MR";
+            mode = "1920x1200";
+            position = "0,0";
+            scale = 1.0;
+          }
+        ];
+
+        exec = [
+          "${pkgs.sway}/bin/swaymsg workspace number 1, move workspace to '\"Hewlett Packard HP E242 CN472419MR\"'"
+          "${pkgs.sway}/bin/swaymsg workspace number 2, move workspace to '\"Hewlett Packard HP E242 CN472419MR\"'"
+          "${pkgs.sway}/bin/swaymsg workspace number 3, move workspace to eDP-1"
+          "${pkgs.sway}/bin/swaymsg workspace number 4, move workspace to eDP-1"
+        ];
+      };
     };
   };
 }
