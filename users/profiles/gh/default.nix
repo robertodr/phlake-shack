cc3cd84b0e772eef4353fa653221373c12c10cb1
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-01-03 16:36:51 +0100

    Leave a comment about gh extensions

--- users/profiles/gh/default.nix
+++ users/profiles/gh/default.nix
@@ -1,5 +1,8 @@
 {...}: {
   programs.gh = {
     enable = true;
+    # for inspiration: https://github.com/kodepandai/awesome-gh-cli-extensions
+    # most of these need to be packaged!
+    # extension = with pkgs; [ gh-eco gh-oblique gh-contrib gh-graph ];
   };
 }
