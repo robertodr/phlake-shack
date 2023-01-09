bbb3b6aab7809e361dcbfdab1ca69e52279e841a
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-01-09 18:43:14 +0100

    Configure wezterm a little bit

--- users/profiles/wezterm/default.nix
+++ users/profiles/wezterm/default.nix
@@ -1,5 +1,15 @@
 {pkgs, ...}: {
   programs.wezterm = {
     enable = true;
+    extraConfig = ''
+      return {
+        font = wezterm.font("Fira Code Nerd Font"),
+        font_size = 14.0,
+        color_scheme = "Blue Matrix",
+        -- color_scheme = "Belge (terminal.sexy)",
+        hide_tab_bar_if_only_one_tab = true,
+        enable_kitty_keyboard = true
+      }
+    '';
   };
 }
