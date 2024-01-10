27eecc7fcd4cf6b7d1381405c1e2af7f4b9dc29d
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-01-10 11:27:22 +0100

    chore: bump nofiles ulimit for the docker daemon

--- systems/profiles/virtualisation/docker/default.nix
+++ systems/profiles/virtualisation/docker/default.nix
@@ -2,6 +2,9 @@
   virtualisation = {
     docker = {
       enable = true;
+      storageDriver = "btrfs";
+      # see here: https://github.com/mamba-org/mamba/issues/2771#issuecomment-1691993724
+      extraOptions = "--default-ulimit nofile=65536:65536";
     };
   };
 }
