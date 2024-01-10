912f27cb1afe73afd2642c3c9fd7b6e92d819121
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-01-10 11:26:45 +0100

    chore: eza alias

--- homes/profiles/eza/default.nix
+++ homes/profiles/eza/default.nix
@@ -3,5 +3,14 @@
     enable = true;
     enableAliases = true;
     git = true;
+    # home-manager makes a mess with quotes generating this alias for fish
+    # it's added under programs.fish.shellAliases
+    #extraOptions = [
+    #  "--binary"
+    #  "--header"
+    #  "--smart-group"
+    #  "--accessed"
+    #  "--group-directories-first"
+    #];
   };
 }
