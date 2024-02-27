2af0793344a93fc91234628eacb271a4a235b8e6
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-02-27 14:22:33 +0100

    feat: add tmpi to PATH, not quite working

---
+++ homes/profiles/tmpi/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.xdg) configHome;
+  inherit (config.lib.dag) entryAfter;
+
+  localBin = "$HOME/.local/bin";
+in {
+  home.sessionPath = ["${localBin}"];
+
+  home.activation.installTmpi = let
+    curl = "$DRY_RUN_CMD ${pkgs.curl}/bin/curl";
+  in
+    entryAfter ["writeBoundary"] ''
+      if [[ ! -f "${localBin}/tmpi" ]]; then
+        mkdir -p ${localBin}
+        ${curl} https://raw.githubusercontent.com/Azrael3000/tmpi/master/tmpi -o ${localBin}/tmpi
+      fi
+    '';
+
+  programs.tmux = {enable = true;};
+}
