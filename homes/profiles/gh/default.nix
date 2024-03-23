2f90ef000ac9ab17e5ad66b785d5608e1b6aea2b
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-03-23 17:12:57 +0100

    feat: add some gh copilot extensions

--- homes/profiles/gh/default.nix
+++ homes/profiles/gh/default.nix
@@ -1,9 +1,6 @@
 {pkgs, ...}: {
   programs.gh = {
     enable = true;
-    # for inspiration: https://github.com/kodepandai/awesome-gh-cli-extensions
-    # most of these need to be packaged!
-    # gh-oblique gh-contrib gh-graph
-    extensions = with pkgs; [gh-eco gh-cal gh-dash gh-actions-cache];
+    extensions = with pkgs; [gh-dash gh-actions-cache unstable.gh-copilot];
   };
 }
--- homes/profiles/vscode/default.nix
+++ homes/profiles/vscode/default.nix
@@ -21,36 +21,38 @@ in {
       "workbench.sideBar.location" = "right";
       # NOTE needed to get past an immediate crash after startup :(
       "window.titleBarStyle" = "custom";
-      "autoDocstring.docstringFormat" = "google-notypes";
-      "python.analysis.diagnosticMode" = "openFilesOnly";
-      # FIXME see below
-      #"cmake.configureOnOpen" = true;
-      "[python]" = {
-        "editor.codeActionsOnSave" = {
-          "source.fixAll" = "explicit";
-          "source.organizeImports" = "explicit";
-        };
-      };
     };
     extensions = with vscode-extensions; [
-      charliermarsh.ruff
-      # NOTE not really using Graphite at the moment
-      #graphite.gti-vscode
+      github.vscode-github-actions
+      github.copilot
       mkhl.direnv
       ms-azuretools.vscode-docker
-      ms-vsliveshare.vsliveshare
-      ms-python.python
-      ms-toolsai.jupyter
       ms-vscode-remote.remote-containers
-      # FIXME requires bleeding edge vscode?
       ms-vscode-remote.remote-ssh
-      # FIXME seems not to be working...
-      #ms-vscode.cmake-tools
-      #ms-vscode.cpptools
-      #ms-vscode.cpptools-extension-pack
-      #ms-vscode.cpptools-themes
-      njpwerner.autodocstring
+      ms-vsliveshare.vsliveshare
       vscodevim.vim
     ];
   };
 }
+
+# Python-related options and extensions
+#"autoDocstring.docstringFormat" = "google-notypes";
+#"python.analysis.diagnosticMode" = "openFilesOnly";
+# FIXME see below
+#"[python]" = {
+#  "editor.codeActionsOnSave" = {
+#    "source.fixAll" = "explicit";
+#    "source.organizeImports" = "explicit";
+#  };
+#};
+#charliermarsh.ruff
+#ms-python.python
+#ms-toolsai.jupyter
+#njpwerner.autodocstring
+
+# C++-related options and extensions
+#"cmake.configureOnOpen" = true;
+#ms-vscode.cmake-tools
+#ms-vscode.cpptools
+#ms-vscode.cpptools-extension-pack
+#ms-vscode.cpptools-themes
