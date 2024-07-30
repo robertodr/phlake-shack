9d8d8ee4ca0019451ee47dd941a825e56adc2a62
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-07-30 16:05:43 +0200

    fix: migrate wireplumber configuration

--- systems/profiles/services/pipewire/default.nix
+++ systems/profiles/services/pipewire/default.nix
@@ -12,49 +12,16 @@
       support32Bit = true;
     };
     pulse.enable = true;
-    wireplumber.enable = true;
-    #media-session.config.bluez-monitor.rules = [
-    #  {
-    #    # Matches all cards
-    #    matches = [{"device.name" = "~bluez_card.*";}];
-    #    actions = {
-    #      "update-props" = {
-    #        # mSBC is not expected to work on all headset + adapter combinations.
-    #        "bluez5.msbc-support" = true;
-    #        # SBC-XQ is not expected to work on all headset + adapter combinations.
-    #        "bluez5.sbc-xq-support" = true;
-    #        "bluez5.reconnect-profiles" = ["hfp_hf" "hsp_hs" "a2dp_sink"];
-    #        # LDAC encoding quality
-    #        # Available values: auto (Adaptive Bitrate, default)
-    #        #                   hq   (High Quality, 990/909kbps)
-    #        #                   sq   (Standard Quality, 660/606kbps)
-    #        #                   mq   (Mobile use Quality, 330/303kbps)
-    #        "bluez5.a2dp.ldac.quality" = "hq";
-    #      };
-    #    };
-    #  }
-    #  {
-    #    matches = [
-    #      # Matches all sources
-    #      {"node.name" = "~bluez_input.*";}
-    #      # Matches all outputs
-    #      {"node.name" = "~bluez_output.*";}
-    #    ];
-    #    actions = {
-    #      "node.pause-on-idle" = false;
-    #    };
-    #  }
-    #];
-  };
-
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '';
+    wireplumber = {
+      enable = true;
+      extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+        };
+      };
+    };
   };
 }
