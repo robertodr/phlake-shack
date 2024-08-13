2f5d5dcacc7df5ad2996667a269ad6b23023463f
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-08-13 10:52:17 +0200

    fix: bluetooth configuration as per wiki page

--- systems/profiles/hardware/bluetooth/default.nix
+++ systems/profiles/hardware/bluetooth/default.nix
@@ -2,7 +2,15 @@
   hardware = {
     bluetooth = {
       enable = true;
-      settings = {General = {Enable = "Source,Sink,Media,Socket";};};
+      powerOnBoot = true;
+      settings = {
+        General = {
+          # https://wiki.nixos.org/wiki/Bluetooth#Enabling_A2DP_Sink
+          Enable = "Source,Sink,Media,Socket";
+          # https://wiki.nixos.org/wiki/Bluetooth#Showing_battery_charge_of_bluetooth_devices
+          Experimental = true;
+        };
+      };
     };
   };
 }
