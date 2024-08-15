5cf03ab231747eca34196ce3cedc5c4f844e13c2
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-08-15 16:02:42 +0200

    chore: add option for thunar

--- systems/profiles/programs/thunar/default.nix
+++ systems/profiles/programs/thunar/default.nix
@@ -1,6 +1,4 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }: {
@@ -11,6 +9,8 @@
       thunar-volman
     ];
   };
+
+  programs.xfconf.enable = true;
 
   services = {
     # Mount, trash, and other functionalities
