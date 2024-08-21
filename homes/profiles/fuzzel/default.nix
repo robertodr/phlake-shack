3681aabb2d45264ebd1de20c9523dae8f8c193d6
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-08-21 17:17:13 +0200

    chore: remove dpi-aware option in fuzzel

--- homes/profiles/fuzzel/default.nix
+++ homes/profiles/fuzzel/default.nix
@@ -1,13 +1,11 @@
 {config, lib, ...}: {
   programs.fuzzel = {
     enable = true;
-    # see: https://codeberg.org/dnkl/fuzzel/src/branch/master/doc/fuzzel.ini.5.scd
     settings = {
       main = {
         terminal = lib.getExe config.programs.kitty.package;
         layer = "overlay";
         icon-theme = "Nordzy-dark";
-        dpi-aware = lib.mkForce "yes";
         width = 50;
       };
     };
