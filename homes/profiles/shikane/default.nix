59c659ed60f7a21f272d854629e6529b6387bce1
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-09-08 17:12:25 +0200

    chore: use 60 Hz refresh for docked configuration

--- homes/profiles/shikane/default.nix
+++ homes/profiles/shikane/default.nix
@@ -54,7 +54,7 @@
           {
             search = "%AOC U34G2G4R3 0x00000E8B";
             enable = true;
-            mode = "3440x1440@120Hz";
+            mode = "3440x1440@60Hz";
             position = "0,0";
             scale = 1.0;
             adaptive_sync = true;
