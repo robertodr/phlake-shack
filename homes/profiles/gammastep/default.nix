c74b9c01704341e849b603c8fb8defbb5e4f00fb
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-12-26 15:59:24 +0100

    chore: gammastep use lat/long instead of geoclue2

--- homes/profiles/gammastep/default.nix
+++ homes/profiles/gammastep/default.nix
@@ -1,7 +1,8 @@
 {
   services.gammastep = {
     enable = true;
-    provider = "geoclue2";
+    latitude = 59.96869229547849;
+    longitude = 10.867898284676471;
     temperature = {
       day = 5500;
       night = 3700;
--- systems/x86_64-linux/kellanved/default.nix
+++ systems/x86_64-linux/kellanved/default.nix
@@ -26,7 +26,6 @@
         "services/earlyoom"
         "services/flatpak"
         "services/fwupd"
-        "services/geoclue2"
         "services/gnome-keyring"
         "services/hardware/bolt"
         "services/languagetool"
