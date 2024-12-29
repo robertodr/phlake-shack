98188ea523e4eec1a152796e7060e3f086f253b5
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2024-12-29 16:47:04 +0100

    fix(blueman-applet): ordering of service start

--- homes/profiles/blueman-applet/default.nix
+++ homes/profiles/blueman-applet/default.nix
@@ -1,5 +1,7 @@
-{
+{lib, ...}: {
   services.blueman-applet = {
     enable = true;
   };
+
+  systemd.user.services.blueman-applet.Unit.After = lib.mkForce ["graphical-session.target" "tray.target"];
 }
