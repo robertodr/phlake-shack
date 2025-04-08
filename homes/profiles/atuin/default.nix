0b12a19a9c381d031398af8a38ee41b525075b70
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-04-08 09:30:46 +0300

    feat(atuin): configure auto sync

--- homes/profiles/atuin/default.nix
+++ homes/profiles/atuin/default.nix
@@ -1,8 +1,18 @@
 {
   programs.atuin = {
     enable = true;
+    # FIXME available in next version
+    #daemon = true;
     settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
       enter_accept = false;
+      update_check = false;
+      sync = {
+        records = true;
+      };
     };
   };
 }
