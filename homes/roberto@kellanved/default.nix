9a0d2e5954d35c500a80f0390d723409186f3b98
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-04-25 09:29:26 +0200

    chore: upgrade stateVersion for home-manager

--- homes/roberto@kellanved/default.nix
+++ homes/roberto@kellanved/default.nix
@@ -57,7 +57,7 @@ in
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "23.11";
+    stateVersion = "24.11";
 
     sessionVariables = {
       EDITOR = "nvim";
