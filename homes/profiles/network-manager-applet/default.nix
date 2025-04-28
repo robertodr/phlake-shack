b9a5e919fb0916def13460c11575738e8c60badd
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-04-28 08:32:49 +0200

    chore(nm-applet): add comment about systemd units

--- homes/profiles/network-manager-applet/default.nix
+++ homes/profiles/network-manager-applet/default.nix
@@ -4,6 +4,7 @@
     enable = true;
   };
 
+  # TODO remove after switching to release-25.05
   systemd.user.services.network-manager-applet.Unit.After = lib.mkForce [
     "graphical-session.target"
     "tray.target"
