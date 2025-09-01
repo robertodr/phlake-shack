de3a92bbcf3e781a122942e1295f370224cb3324
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-09-01 09:51:15 +0200

    chore: re-lock flake

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
     };
 
     stylix = {
-      url = "github:danth/stylix/release-25.05";
+      url = "github:nix-community/stylix/release-25.05";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
