bf490b9fac227be41ef5bac15c790c91f15bde07
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-09-16 14:15:20 +0200

    fix: remove useless configuration

--- systems/profiles/hardware/bluetooth/default.nix
+++ systems/profiles/hardware/bluetooth/default.nix
@@ -4,14 +4,6 @@
     bluetooth = {
       enable = true;
       powerOnBoot = true;
-      settings = {
-        General = {
-          # https://wiki.nixos.org/wiki/Bluetooth#Enabling_A2DP_Sink
-          Enable = "Source,Sink,Media,Socket";
-          # https://wiki.nixos.org/wiki/Bluetooth#Showing_battery_charge_of_bluetooth_devices
-          Experimental = true;
-        };
-      };
     };
   };
 }
