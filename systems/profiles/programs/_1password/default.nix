2199d5b22fb5ad5c512d57c161daa16c23cbeb15
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-10-25 15:52:14 +0200

    feat: use 1password gui beta and cli from unstable

--- systems/profiles/programs/_1password/default.nix
+++ systems/profiles/programs/_1password/default.nix
@@ -1,11 +1,14 @@
+{ pkgsUnstable, ... }:
 {
   programs = {
     _1password = {
       enable = true;
+      package = pkgsUnstable._1password-cli;
     };
 
     _1password-gui = {
       enable = true;
+      package = pkgsUnstable._1password-gui-beta;
       polkitPolicyOwners = [ "roberto" ];
     };
   };
