085604ae2d91090cc8af2c9a24e6de6684a4217a
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2025-12-29 17:55:27 +0100

    chore: lxqt-policykit in home

--- systems/profiles/programs/_1password/default.nix
+++ systems/profiles/programs/_1password/default.nix
@@ -12,24 +12,4 @@
       polkitPolicyOwners = [ "roberto" ];
     };
   };
-
-  systemd.user = {
-    services = {
-      # needed to get 1password to use system authentication correctly:
-      # https://1password.community/discussion/comment/634787/#Comment_634787
-      lxqt-policykit-agent = {
-        description = "lxqt-policykit-agent";
-        wantedBy = [ "graphical-session.target" ];
-        wants = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
-    };
-  };
 }
