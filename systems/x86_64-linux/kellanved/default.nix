5cbd0ff8b7a30e8a4d629e8b35fad263d2c8d904
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-01-01 21:11:20 +0100

    fix: persist host SSH keys

--- systems/x86_64-linux/kellanved/default.nix
+++ systems/x86_64-linux/kellanved/default.nix
@@ -224,6 +224,10 @@
             mode = "u=rwx,g=,o=";
           };
         }
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
       ];
     };
 
