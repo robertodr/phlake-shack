6150157945e34e70211ab1127b7e7e372d17e84b
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-05-12 20:11:10 +0200

    feat(vicinae): add a bunch of extensions

--- homes/profiles/vicinae/default.nix
+++ homes/profiles/vicinae/default.nix
@@ -3,34 +3,32 @@
   pkgs,
   ...
 }:
+let
+  rev = "d12bcb134d45dedad1a28a18e1cd8807353338d0";
+  sha256 = "sha256-/Yx97SOwvJXPxvv6e81j+ID87J9DQGq0QGYnmsWKoz8=";
+  mkExt =
+    name:
+    config.lib.vicinae.mkExtension {
+      inherit name;
+      src =
+        pkgs.fetchFromGitHub {
+          owner = "vicinaehq";
+          repo = "extensions";
+          inherit rev sha256;
+        }
+        + "/extensions/${name}";
+    };
+in
 {
   programs.vicinae = {
     enable = true;
     systemd.enable = true;
-    extensions = [
-      (config.lib.vicinae.mkExtension {
-        name = "nix";
-        src =
-          pkgs.fetchFromGitHub {
-            owner = "vicinaehq";
-            repo = "extensions";
-            rev = "e01fe274f037e4d2b7436718258fa898f80dc4b2";
-            sha256 = "sha256-/Yx97SOwvJXPxvv6e81j+ID87J9DQGq0QGYnmsWKoz8=";
-          }
-          + "/extensions/nix";
-      })
-
-      (config.lib.vicinae.mkExtension {
-        name = "bluetooth";
-        src =
-          pkgs.fetchFromGitHub {
-            owner = "vicinaehq";
-            repo = "extensions";
-            rev = "e01fe274f037e4d2b7436718258fa898f80dc4b2";
-            sha256 = "sha256-/Yx97SOwvJXPxvv6e81j+ID87J9DQGq0QGYnmsWKoz8=";
-          }
-          + "/extensions/bluetooth";
-      })
+    extensions = map mkExt [
+      "nix"
+      "bluetooth"
+      "player-pilot"
+      #"systemd"
+      "pulseaudio"
     ];
   };
 }
