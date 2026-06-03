cc29c6d9cb39c7248690c01a26c245f5b711bbd5
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-06-03 11:25:31 +0200

    fix: feedr module

--- homes/profiles/feedr/default.nix
+++ homes/profiles/feedr/default.nix
@@ -5,87 +5,87 @@
       default_feeds = [
         {
           url = "https://arxiv.org/rss/physics.chem-ph";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://arxiv.org/rss/physics.comp-ph";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://arxiv.org/rss/cond-mat.str-el";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.aip.org/rss/site_1000043/1000024.xml";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.aip.org/rss/site_1000043/LatestOpenIssueArticles_1000024.xml";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=achre4";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=chreay";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jctcce";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jceda8";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jpcafh";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jpcbfk";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jpccck";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jpclcd";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://onlinelibrary.wiley.com/action/showFeed?jc=1520636x&type=etoc&feed=rss";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://onlinelibrary.wiley.com/action/showFeed?jc=15213773&type=etoc&feed=rss";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://onlinelibrary.wiley.com/action/showFeed?jc=17590884&type=etoc&feed=rss";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://onlinelibrary.wiley.com/action/showFeed?jc=1097461x&type=etoc&feed=rss";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://onlinelibrary.wiley.com/action/showFeed?jc=1096987x&type=etoc&feed=rss";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://rss.sciencedirect.com/publication/science/00104655";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "https://www.tandfonline.com/action/showFeed?type=etoc&feed=rss&jc=tmph20";
-          category = "Science"
+          category = "Science";
         }
         {
           url = "http://feeds.rsc.org/rss/CP?format=xml";
-          category = "Science"
+          category = "Science";
         }
       ];
     };
