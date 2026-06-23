3d73167cc1d56ce44f0f9889cc99837095454fa3
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-06-23 10:10:29 +0200

    chore(ghostty): enable copy-on-select

--- homes/profiles/ghostty/default.nix
+++ homes/profiles/ghostty/default.nix
@@ -8,6 +8,7 @@
     settings = {
       gtk-tabs-location = "bottom";
       shell-integration-features = "ssh-terminfo,ssh-env";
+      copy-on-select = true;
     };
     systemd.enable = true;
   };
