c0cd1009f6c89fc70844a43f172793935f258ab8
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-08-10 09:15:04 +0200

    chore(claude): add global configuration

--- homes/profiles/claude-code/default.nix
+++ homes/profiles/claude-code/default.nix
@@ -3,5 +3,14 @@
   programs.claude-code = {
     enable = true;
     package = pkgsUnstable.claude-code;
+    context = ''
+    If you make a commit, follow conventional commits and add a trailer:
+`Assisted-by: <harness>:<model>`, where `<harness>` is the current agent harness
+(like ClaudeCode), and `<model>` is the AI model (Like claude-opus-4.8). You
+don't need to add a coauthored-by Claude when you have this.
+
+Prefix PR descriptions and comments on PRs with the line ":robot: _AI text
+below_ :robot:" to indicate you are an agent speaking on a user's behalf.
+    '';
   };
 }
