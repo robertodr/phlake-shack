9bd004ddb8bb82080fb26f6fb8713cb34556cfa3
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-09-12 11:04:26 +0200

    feat(helix): complete configuration

--- homes/profiles/helix/default.nix
+++ homes/profiles/helix/default.nix
@@ -1,9 +1,80 @@
-{ pkgs, pkgsUnstable, ... }:
+{
+  pkgs,
+  pkgsUnstable,
+  ...
+}:
 {
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    #extraPackages = [];
-    #languages = {};
+
+    extraPackages = with pkgs; [
+      # Nix
+      nixd
+      nixfmt
+
+      # Rust
+      rust-analyzer
+      rustfmt
+      lldb
+
+      # C/C++
+      clang-tools
+
+      # CMake
+      pkgsUnstable.neocmakelsp
+      pkgsUnstable.gersemi
+
+      # Python
+      ty
+      ruff
+
+      # Typst
+      pkgsUnstable.typst
+      pkgsUnstable.tinymist
+      pkgsUnstable.typstyle
+    ];
+
+    languages.language = [
+      {
+        name = "nix";
+        language-servers = [ "nixd" ];
+        formatter.command = "nixfmt";
+        auto-format = true;
+      }
+      {
+        name = "rust";
+        language-servers = [ "rust-analyzer" ];
+        auto-format = true;
+      }
+      {
+        name = "cpp";
+        language-servers = [ "clangd" ];
+        auto-format = true;
+      }
+      {
+        name = "cmake";
+        language-servers = [ "neocmakelsp" ];
+        formatter = {
+          command = "gersemi";
+          args = [ "-" ];
+        };
+        auto-format = true;
+      }
+      {
+        name = "python";
+        language-servers = [
+          "ty"
+          "ruff"
+        ];
+        auto-format = true;
+      }
+      {
+        name = "typst";
+        language-servers = [ "tinymist" ];
+        formatter.command = "typstyle";
+        auto-format = true;
+      }
+    ];
   };
 }
