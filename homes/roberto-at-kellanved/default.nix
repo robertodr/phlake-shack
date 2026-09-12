f1083685ec5ce44f656d4ba5f8369189f4fc6d48
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-09-12 11:06:00 +0200

    chore: some more packages from llm-agents

--- homes/roberto-at-kellanved/default.nix
+++ homes/roberto-at-kellanved/default.nix
@@ -144,7 +144,9 @@ in
       jless
       llm-agents.but
       llm-agents.codegraph
+      llm-agents.coderabbit-cli
       llm-agents.gitbutler
+      llm-agents.qmd
       llm-agents.skills
       meld
       perf
